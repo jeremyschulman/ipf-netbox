1d360b1c0df593484859070cc3d1f20afb356105
Jeremy Schulman <jeremy.schulman@mlb.com>
2020-09-03 13:26:05 -0400

    update

--- ipfnbtk/cli/__init__.py
+++ ipfnbtk/cli/__init__.py
@@ -1,4 +1,4 @@
-from . import cli_snapshot  # noqa
+from . import cli_snapshot_devices  # noqa
 from . import cli_audit_devices  # noqa
 from . import cli_audit_serial_numbers  # noqa
 from . import cli_reconcile_serial_numbers  # noqa
